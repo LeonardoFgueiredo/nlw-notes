9f9af4d2df9a99c9a72210ad2b6a53ecc991787f
Leonardo Figueiredo <leodbf@hotmail.com>
2024-02-06 13:30:08 -0300

    style: adicionando flex flex-col no card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,10 @@
 export function NoteCard() {
     return (
-      <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+      <button className="rounded-md text-left bg-slate-800 p-5 flex flex-col gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
         <span className="text-sm font-medium text-slate-300">há 2 dias</span>
   
         <p className="text-sm leading-6 text-slate-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
-          dolor quaerat similique asperiores minus vitae magni nemo animi iusto
-          ducimus sapiente, error placeat ratione vel. Voluptatibus sequi quis
-          ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-          commodi dolor quaerat similique asperiores minus vitae magni nemo animi
-          iusto ducimus sapiente, error placeat ratione vel. Voluptatibus sequi
-          quis ratione.
+          Ir para a academia 🏋️
         </p>
   
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
